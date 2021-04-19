9890ba1498f45aa5d45c05f544c93f4399bb80e2
serega11021995 <segoopasniy1995@gmail.com>
2021-04-19 13:39:17 +0300

    git add .

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import state, { obscribe } from './redux/state';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-import { addPost, updatePostText } from './redux/state'
+import store from './redux/state'
 
 
-let rerender = () => {
+
+let rerender = (state) => {
       ReactDOM.render(
             <BrowserRouter>
-                  <App state={state} addPost={addPost} updatePostText={updatePostText} />
+                  <App state={state} addPost={store.addPost.bind(store)} updatePostText={store.updatePostText.bind(store)} />
             </BrowserRouter>
             , document.getElementById('root')
       );
@@ -20,9 +20,9 @@ let rerender = () => {
 
 
 
-rerender(state)
+rerender(store.getState())
 
-obscribe(rerender)
+store.obscribe(rerender)
 
 
 
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,66 +1,70 @@
-let rerender = () =>{
+let store = {
+  getState(){
+    return this._state
+  },
+  _state : {
+
+    profilePage: {
   
-}
-
-
-let state = {
-
-  profilePage: {
-
-    newPostText: '',
-    postData: [
-      { id: 1, message: "Hi World", like: 10 },
-      { id: 2, message: "Its my first post", like: 22 }
-    ],
-
+      newPostText: '',
+      postData: [
+        { id: 1, message: "Hi World", like: 10 },
+        { id: 2, message: "Its my first post", like: 22 }
+      ],
+  
+    },
+  
+    dialogsPage: {
+      dialogData: [
+        { 'id': 1, 'name': 'Валентин', },
+        { 'id': 2, 'name': 'Аедрей', },
+        { 'id': 3, 'name': 'Ашот', },
+        { 'id': 4, 'name': 'Станислав', },
+        { 'id': 5, 'name': 'Янис', },
+      ],
+      messagesData: [
+        { 'id': 1, 'messege': 'Привет, как ты??' },
+        { 'id': 2, 'messege': 'Пойдет, ты как брат??' },
+        { 'id': 3, 'messege': 'Нормально! Спасибо' },
+      ],
+    },
+  
+    sidebar: {
+      navdata: [
+        { 'url': '/Profile', 'name': 'Профиль' },
+        { 'url': '/Dialogs', 'name': 'Сообщения' },
+        { 'url': '/News', 'name': 'Новости' },
+        { 'url': '/Music', 'name': 'Музыка' },
+      ]
+    },
+  
+  
+  },
+  _callbackSubscriber(){
+    console.log('State changed')
   },
 
-  dialogsPage: {
-    dialogData: [
-      { 'id': 1, 'name': 'Валентин', },
-      { 'id': 2, 'name': 'Аедрей', },
-      { 'id': 3, 'name': 'Ашот', },
-      { 'id': 4, 'name': 'Станислав', },
-      { 'id': 5, 'name': 'Янис', },
-    ],
-    messagesData: [
-      { 'id': 1, 'messege': 'Привет, как ты??' },
-      { 'id': 2, 'messege': 'Пойдет, ты как брат??' },
-      { 'id': 3, 'messege': 'Нормально! Спасибо' },
-    ],
+  addPost (newPost) {
+    let post = {
+      id: 5,
+      message: newPost,
+      like: 0
+    }
+    this._state.profilePage.postData.push(post)
+    this._state.profilePage.newPostText = ''
+    this._callbackSubscriber(this._state)
   },
 
-  sidebar: {
-    navdata: [
-      { 'url': '/Profile', 'name': 'Профиль' },
-      { 'url': '/Dialogs', 'name': 'Сообщения' },
-      { 'url': '/News', 'name': 'Новости' },
-      { 'url': '/Music', 'name': 'Музыка' },
-    ]
+  updatePostText(text){
+    this._state.profilePage.newPostText = (text)
+    this._callbackSubscriber(this._state)
   },
 
-
-};
-
-export const obscribe = (observer) => {
-  rerender = observer
-}
-
-export const addPost = (newPost) => {
-  let post = {
-    id: 5,
-    message: newPost,
-    like: 0
+  obscribe (observer) {
+    this._callbackSubscriber = observer
   }
-  state.profilePage.postData.push(post)
-  state.profilePage.newPostText = ''
-  rerender(state)
+
 }
 
 
-export const updatePostText = (text) => {
-  state.profilePage.newPostText = (text)
-  rerender(state)
-}
-
-export default state;
+export default store;
